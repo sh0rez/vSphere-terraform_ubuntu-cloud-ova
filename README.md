3f2a3a7660ac9f0226b067484f263e99975351ef
Elsvent Hong <Elsvent.hong@prophetstor.com>
2018-12-03 10:24:11 +0800

    Using ISO pre-install template

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# :cloud: vSphere Terraform Ubuntu-cloud OVA/OVF
-This repository contians an example how to deploy an cloud-init configurable Ubuntu-cloud OVA to VMware vSphere using Terraform
+# vSphere terraform unify os for customize ip hostname setting
+This repository fork from sh0rez/vSphere-terraform_ubuntu-cloud-ova
 
-**It is [part of a blog post](https://shorez.de/2018/deploying-ubuntu-cloud-images-to-vsphere-using-terraform/) , consider reading it for further instructions**
+The original repository using ova package, this repository using ISO pre-install
+template for customize.
+
+Some point you may need to know before using vSphere plugin
+
+1. vmware tool is required for clone customize
+2. perl is required(if you search you will see a lot of user failed because perl
+3. please aware the support matrix, some essential OS may failed the guestOS check.
+
+Note: if you are using CentOS, sometime may failed because /etc/redhat-release not match.
+There is a trick hint: for me using **Red Hat Enterprise Linux Server release 7.0 (Maipo)**
+, it will work and not effect function.
 
 # License :book:
 Released into the public domain under terms of the [UNLICENSE](/LICENSE).
-Anyways, pull requests and contribution is desired :smile:
+Any PR will be welcome.:)
--- terraform.tf
+++ terraform.tf
@@ -31,12 +31,13 @@ data "vsphere_virtual_machine" "template" {
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "${var.name}"
+  count = "${var.vm_numbers}"
+  name             = "${var.name}-${count.index+1}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-  num_cpus = 2
-  memory   = 1024
+  num_cpus = 4
+  memory   = 8192
   guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
 
   scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
@@ -57,13 +58,22 @@ resource "vsphere_virtual_machine" "vm" {
     client_device = true
   }
 
-  vapp {
-    properties {
-      user-data = "${base64encode(file("cloud-init.yml"))}"
-    }
-  }
-
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    customize {
+      linux_options {
+        host_name = "${var.name}${count.index+1}"
+        domain    = "${var.virtual_machine_domain}"
+      }
+
+      network_interface {
+        ipv4_address = "${cidrhost(var.virtual_machine_network_address, var.virtual_machine_ip_address_start + count.index)}"
+        ipv4_netmask = "${element(split("/", var.virtual_machine_network_address), 1)}"
+      }
+
+      ipv4_gateway    = "${var.virtual_machine_network_gateway}"
+      dns_suffix_list = ["${var.virtual_machine_domain}"]
+      dns_server_list = ["${var.virtual_machine_dns_servers}"]
+    }
   }
 }
